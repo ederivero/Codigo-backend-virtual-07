ebf89ceb235b42dc027174f9227fe6382e6fab31
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-28 18:44:38 -0500

    ejercicio en simulador

--- Dia4/BD Empresa.sql
+++ Dia4/BD Empresa.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS EMPRESA;
 CREATE DATABASE IF NOT EXISTS EMPRESA;
 use EMPRESA;
 # Crear una Base de Datos para la gestión de los empleados de una empresa, en la cual está distribuida por departamenteos (Informática, Publicidad, Marketing, Créditos)
@@ -5,25 +6,24 @@ use EMPRESA;
 # su nivel (en que piso de la empresa se encuentra)
 # NOTA: No todos los empleados tiene departamento.
 CREATE TABLE IF NOT EXISTS departamentos(
-	id int not null unique auto_increment primary key,
+    id int not null unique auto_increment primary key,
     nombre varchar(50),
     piso int
 );
 CREATE TABLE IF NOT EXISTS personales(
-	id int not null unique primary key auto_increment,
+    id int not null unique primary key auto_increment,
     nombre varchar(50),
     apellido varchar(50),
-    identificador int,
+    identificador text,
     departamento_id int,
     supervisor_id int,
     constraint departamentos_personales foreign key (departamento_id) references departamentos(id),
     constraint personales_personales foreign key (supervisor_id) references personales(id)
 );
-
-INSERT INTO departamentos (nombre, piso)VALUES 
-						 ('Ventas',1),
-						 ('Administracion',2),
-                         ('Finanzas',2),
-                         ('Marketing',3);
-                         
-                         
+INSERT INTO departamentos (nombre, piso)
+VALUES ('Ventas', 1),
+    ('Administracion', 2),
+    ('Finanzas', 2),
+    ('Marketing', 3);
+SELECT *
+from departamentos;
--- Dia4/simulador.py
+++ Dia4/simulador.py
@@ -1,4 +1,21 @@
 from faker import Faker
-
+from faker.providers import internet
 fake = Faker()
-print(fake.profile())
+fake.add_provider(internet)
+# GENERA GRACIAS AL PROVIDE DE internet, una imagen cuyo ancho y alto sera de 100px
+print(fake.image_url(width=100, height=100))
+# GENERAR 500 empleados
+print(fake.unique.first_name())
+# GENERA UN APELLIDO ALEATORIO
+print(fake.last_name())
+# GENERA UN CORREO ALEATORIO
+print(fake.email())
+# GENERA UN NOMBRE ALEATORIO
+print(fake.first_name())
+# GENERA UN NUMERO RANDOM ALEATORIO
+print(fake.random_int(min=1, max=501))
+print(fake.uuid())
+
+# GENERAR DATA SIMULADA DE 500 personales
+# 670b9562-b30d-52d5-b827-655787665500
+# INSERT INTO PERSONALES VALUES (1, "EDUARDO", "DE RIVERO", uuid, 2, null);
