a11e4f3afe613d6c2736d15e3f45308d8454d598
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-29 13:03:32 -0500

    avance bd flask

---
+++ Dia6/flask_con_bd.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+# esto sirve para que si tenemos un archivo .env jale todas las variables como si fuesen variables de entorno
+from dotenv import load_dotenv
+from os import environ
+load_dotenv()
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = environ.get("HOST")
+app.config['MYSQL_USER'] = environ.get("USER")
+app.config['MYSQL_PASSWORD'] = environ.get("PASSWORD")
+app.config['MYSQL_DB'] = environ.get("DATABASE")
+app.config['MYSQL_PORT'] = int(environ.get("PORT"))
+
+# Crearmos una instancia de la clase MySQL y le pasamos a su constructor la configuracion
+mysql = MySQL(app)
+
+# ALTER USER 'username'@'url' IDENTIFIED WITH mysql_native_password BY 'password';
+# si es que nos sale un error de conexion
+
+
+@app.route("/alumnos")
+def gestion_alumnos():
+    # primero creo mi cursor que se conectara a la bd
+    cur = mysql.connection.cursor()
+    # registro la sentencia ya sea un DDL o DML
+    cur.execute("SELECT * FROM ALUMNOS")
+    # capturo la informacion a partir de la consulta
+    data = cur.fetchall()
+    return {
+        "data": data
+    }
+
+
+app.run(debug=True)
