db2733d1e670a5f572fd30e4ac193e001739e9ef
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-06-23 18:17:57 -0500

    limpieza de la rama

--- blog_personal/blog_personal/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('panel-administrativo/', admin.site.urls),
-    path('api/', include('gestion.urls'))
-]
