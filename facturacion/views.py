28e21461abbff9167bf1e4f5f439f3efa5958454
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-06-29 19:43:43 -0500

    avance facturacion

--- facturacion/views.py
+++ facturacion/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+
+class ComprobanteController(CreateAPIView):
+    def post(self, request):
+        # mandar por el body => {
+        # pedidoId,
+        # tipoComprobante,
+        # observaciones,
+        # }
+        # Validate => validar que el pedido exista, que el tipoComprobante sea 1 o 2 y las obs puedan ser opcionales
+        pass
