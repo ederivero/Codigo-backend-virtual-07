0f2a20318374482e9fa4fc2898be0287131623c1
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-07-13 20:59:29 -0500

    modificacion dialectOptions

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const opciones: Options = {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
   timezone: "-05:00",
   // sirve para indicar si queremos o no queremos ver la query en la consola
   logging: false,
-  ssl: false,
+  ssl: true,
 };
 
 export default new Sequelize(String(process.env.DATABASE_URL), opciones);
