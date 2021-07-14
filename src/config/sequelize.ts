1822142fef521d7d1207ee243f3ca6b5fd566b60
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-07-13 21:43:47 -0500

    modificaciones archivo server y sequelize

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -2,13 +2,13 @@ import { Sequelize, Options } from "sequelize";
 require("dotenv").config();
 
 const opciones: Options = {
-  dialectOptions: !process.env.NODE_ENV
-    ? {
+  dialectOptions: process.env.NODE_ENV
+    ? {}
+    : {
         ssl: {
           rejectUnauthorized: false,
         },
-      }
-    : {},
+      },
   timezone: "-05:00",
   // sirve para indicar si queremos o no queremos ver la query en la consola
   logging: false,
