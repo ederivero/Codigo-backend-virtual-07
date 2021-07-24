5109ec07a4045b53de4e097e99588b71f95ba346
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-07-23 22:04:49 -0500

    creacion de mp proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var port = process.env.PORT || 3000
+
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', req.query);
+});
+
+app.listen(port);
