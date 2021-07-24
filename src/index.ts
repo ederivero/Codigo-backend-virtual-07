2b7bf21cd9984a743e0560d30d3af9c03e3887f7
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-07-23 21:42:28 -0500

    limpieza de la rama

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import Server from "./config/server";
-
-const objServidor = new Server();
-objServidor.start();
-
-console.log(__dirname);
--- src/usuario/usuario.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { registroDto } from "./dto.request";
-import { login, registro } from "./usuario.controller";
-
-export const usuarioRouter = Router();
-usuarioRouter.post("/registro", registroDto, registro);
-usuarioRouter.post("/login", login);
