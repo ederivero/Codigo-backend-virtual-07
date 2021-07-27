4eaf4cdb778c0caaf4442eb47b458155a3fbaf8a
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-07-26 22:02:59 -0500

    avance socket

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -5,6 +5,9 @@ import { createServer, Server as HTTPServer } from "http";
 interface IRegistro {
   username: string;
 }
+interface IUsuario extends IRegistro {
+  id: string;
+}
 
 export default class Server {
   app: Express;
@@ -35,12 +38,38 @@ export default class Server {
   escucharSockets() {
     // el metodo on se ejecutara cuando el cliente envie ese evento
     // nosotros podemos crear los eventos que querramos PEEEERO hay metodos ya creados que no se pueden modificar
+    let usuarios: Array<IUsuario> = [];
     this.io.on("connect", (cliente) => {
       console.log(`Se conecto el cliente! ${cliente.id}`);
 
       cliente.on("registrar", (objCliente: IRegistro) => {
-        console.log(objCliente);
+        // validamos si ese usuario no fue registrado previamente
+        const usuarioEncontrado = usuarios.filter(
+          (usuario) => usuario.id === cliente.id
+        )[0];
+        if (!usuarioEncontrado) {
+          usuarios.push({
+            username: objCliente.username,
+            id: cliente.id,
+          });
+          console.log(usuarios);
+          this.io.emit("lista-usuarios", usuarios);
+        }
       });
+
+      cliente.on("disconnect", (razon) => {
+        console.log(razon);
+        // cuando el usuario se desconecte, retirarlo de la lista de usuarios
+        usuarios = usuarios.filter((usuario) => usuario.id !== cliente.id);
+        console.log(usuarios);
+        // estamos haciendo un broadcast estamos enviando el evento a todos los usuarios conectados
+        this.io.emit("lista-usuarios", usuarios);
+        console.log(`Se desconecto el usuario ${cliente.id}`);
+      });
+      // si nosotros queremos hacer la emision de un evento pero solamente al usuario que la ha solicitado entonces se realizara mediante ese cliente
+      cliente.emit("lista-usuarios", usuarios);
+      // cuando nosotros queremos emitir un evento a todos los demas usuarios EXCEPTO al usuario conectado entonces haremos un broadcast
+      //  cliente.broadcast.emit("lista-usuarios", usuarios);
     });
   }
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,14 @@
 const socket = io("http://127.0.0.1:8000");
 const btnIngresar = document.getElementById("btnIngresar");
 const username = document.getElementById("username");
+const usuarios_conectados = document.getElementById("usuarios-conectados");
 
 btnIngresar.addEventListener("click", (evento) => {
   socket.emit("registrar", { username: username.value });
+});
+
+socket.on("lista-usuarios", (usuarios) => {
+  usuarios.forEach((usuario) => {
+    console.log(usuario);
+  });
 });
